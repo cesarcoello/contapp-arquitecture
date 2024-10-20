10a7d1eb73b994fef03d62a743b33dc0a5090bd7
César Jarith Coello <jarith182@gmail.com>
2024-10-19 22:18:47 -0600

    diagrama de arquitectura

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 provider "azurerm" {
   features {}
-  subscription_id = "ac08f0b0-e258-491e-802e-eb2b78eebb46"  # Reemplaza esto con tu ID de suscripción
 }
 
 resource "azurerm_resource_group" "rg" {
